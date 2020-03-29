c96d0f58956363b5701f8f644f1933857fba23c0
berkansems <berkansems@gmail.com>
2020-03-29 11:15:23 +0300

    second_version

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from accounts.models import Customer,Product,Tag,Order
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Tag)
-admin.site.register(Order)
