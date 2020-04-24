306e0656c1fdc10d00bab1f6bf4748a7406b54d0
berkansems <berkansems@gmail.com>
2020-04-24 17:13:53 +0300

    prepared for docker

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=20, null=True)
+
     email = models.CharField(max_length=200, null=True)
     dateCreated = models.DateTimeField(auto_now_add=True, null=True)
     profilePicture = models.ImageField(null=True, blank=True)
